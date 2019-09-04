8c6650438ef493ab76a5c193d0fe9ad35d7de692
wyattelijah <wyatt.elijahd@gmail.com>
2019-09-04 03:55:02 -0400

    completed main.py

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('Hello, my name is Elijah Wyatt!')
+print('My favorite game is Crash Bandicoot')
+print("I don't really have any concerns with this class")
+print('I am excited to learn about game technologies and how games are made')
+print('My stack overflow user number is 12018400')
+print('The url to my github profile ishttps://github.com/wyattelijah')
